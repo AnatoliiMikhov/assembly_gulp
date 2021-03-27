3f0382cbb79d5e8954deba6c04edc210907d34bc
AnatoliiMikhov <anatoli.mihov@gmail.com>
2021-03-27 21:42:25 +0200

    checkWebpSupport function

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
 import modals from './modules/modals';
+import checkWebpSupport from './checkWebpSupport';
 
 window.addEventListener('DOMContentLoaded', () => {
 	'use strict';
-
 	console.log('test script.js');
+
+	checkWebpSupport();
 
 	modals();
 });
